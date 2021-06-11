c693762c2d31822b421e2f25a2433ad99fb1c371
Songleen <sellee7092@163.com>
2021-06-11 16:57:49 +0800

    新建了一个文件

---
+++ src/main/java/temp/Person.java
@@ -0,0 +1,13 @@
+package temp;
+
+/**
+ * @Describe
+ * @ClassName
+ * @Author 李松林
+ * @Date 2021/6/11 16:56
+ */
+public class Person {
+    public static void main(String[] args) {
+        System.out.println("新建了一个类");
+    }
+}
